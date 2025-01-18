4b9b7014614c5c0e068fbd2229ed9f6ffbc1ccdb
Yuval hermon <126855328+cptgingerbeard@users.noreply.github.com>
2025-01-18 09:33:30 +0000

    feat: add Angular Material and CDK dependencies, update package-lock

--- OnoAirapp2/src/app/menu/menu.component.ts
+++ OnoAirapp2/src/app/menu/menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
 
 
 @Component({
   selector: 'app-menu',
-  imports: [ RouterModule],
+  imports: [ RouterModule, MatListModule ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
